cc5c30e75d63a3d4f7781f49e919c0c0a012a1b7
Ethan Hanks <ethanjhanks99@gmail.com>
2026-05-09 08:42:58 -0600

    fixed type tests, added math ops

--- src/tests/lexer/lexer_tests.cpp
+++ src/tests/lexer/lexer_tests.cpp
@@ -27,49 +27,379 @@ TEST_CASE("Lexer tokenizes string literals", "[lexer]") {
   REQUIRE(tokens[0].lexeme == "Hello World");
 }
 
-TEST_CASE("Lexer tokenizes single character tokens") {
-  auto chars = GENERATE("(", ")", "[", "]", "{", "}", ";", ",", ".", ":", "?");
-  auto types =
-      GENERATE(TokenType::LPAREN, TokenType::RPAREN, TokenType::LBRACKET,
-               TokenType::RBRACKET, TokenType::LBRACE, TokenType::RBRACE,
-               TokenType::SEMICOLON, TokenType::COMMA, TokenType::DOT,
-               TokenType::COLON, TokenType::QMARK);
+TEST_CASE("Lexer tokenizes single character tokens", "[lexer]") {
+  SECTION("Left Parenthese") {
+    Lexer lexer("(");
+    auto tokens = lexer.scan_tokens();
 
-  Lexer lexer(chars);
-  auto tokens = lexer.scan_tokens();
+    REQUIRE(tokens[0].type == TokenType::LPAREN);
+  }
 
-  REQUIRE(tokens[0].type == types);
+  SECTION("Right Parenthase") {
+    Lexer lexer(")");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RPAREN);
+  }
+
+  SECTION("Left Bracket") {
+    Lexer lexer("[");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::LBRACKET);
+  }
+
+  SECTION("Right Bracket") {
+    Lexer lexer("]");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RBRACKET);
+  }
+
+  SECTION("Left Brace") {
+    Lexer lexer("{");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::LBRACE);
+  }
+
+  SECTION("Right Brace") {
+    Lexer lexer("}");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RBRACE);
+  }
+
+  SECTION("Semicolon") {
+    Lexer lexer(";");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::SEMICOLON);
+  }
+
+  SECTION("Comma") {
+    Lexer lexer(",");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::COMMA);
+  }
+
+  SECTION("Dot") {
+    Lexer lexer(".");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::DOT);
+  }
+
+  SECTION("Colon") {
+    Lexer lexer(":");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::COLON);
+  }
+
+  SECTION("Question Mark") {
+    Lexer lexer("?");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::QMARK);
+  }
+}
+
+TEST_CASE("Lexer tokenizes multi-character tokens", "[lexer]") {
+  SECTION("Return arrow") {
+    Lexer lexer("->");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RETURN_POINT);
+  }
+
+  SECTION("Double Colon") {
+    Lexer lexer("::");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::COLON_COLON);
+  }
+
+  SECTION("Match Arrow") {
+    Lexer lexer("=>");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MATCH_ARROW);
+  }
+
+  SECTION("Range (non-inclusive)") {
+    Lexer lexer("..");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RANGE);
+  }
+
+  SECTION("Range (inclusive)") {
+    Lexer lexer("..=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RANGE_INCLUSIVE);
+  }
+}
+
+TEST_CASE("Lexer tokenizes logical operators", "[lexer]") {
+  SECTION("Assignment") {
+    Lexer lexer(":=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::ASSIGN);
+  }
+
+  SECTION("Compare Equal") {
+    Lexer lexer("==");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::EQUAL);
+  }
+
+  SECTION("Not Equal") {
+    Lexer lexer("!=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::NOT_EQUAL);
+  }
+
+  SECTION("Less than") {
+    Lexer lexer("<");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::LESS);
+  }
+
+  SECTION("Less than or equal too") {
+    Lexer lexer("<=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::LESS_EQUAL);
+  }
+
+  SECTION("Greater than") {
+    Lexer lexer(">");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::GREAT);
+  }
+
+  SECTION("Greather than or equal too") {
+    Lexer lexer(">=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::GREAT_EQUAL);
+  }
+
+  SECTION("And") {
+    Lexer lexer("&&");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::AND);
+  }
+
+  SECTION("Or") {
+    Lexer lexer("||");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::OR);
+  }
+
+  SECTION("Not") {
+    Lexer lexer("!");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::NOT);
+  }
+}
+
+TEST_CASE("Lexer tokenizes Mathematical and Bit Operators", "[lexer]") {
+  SECTION("Addition") {
+    Lexer lexer("+");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::PLUS);
+  }
+
+  SECTION("Subtraction") {
+    Lexer lexer("-");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MINUS);
+  }
+
+  SECTION("Multiplication") {
+    Lexer lexer("*");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::STAR);
+  }
+
+  SECTION("Division") {
+    Lexer lexer("/");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::SLASH);
+  }
+
+  SECTION("Power") {
+    Lexer lexer("**");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::POWER);
+  }
+
+  SECTION("Module") {
+    Lexer lexer("%");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MODULO);
+  }
+
+  SECTION("Plus equals") {
+    Lexer lexer("+=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::PLUS_EQUALS);
+  }
+
+  SECTION("Minus equals") {
+    Lexer lexer("-=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MINUS_EQUALS);
+  }
+
+  SECTION("Multiply equals") {
+    Lexer lexer("*=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MULT_EQUALS);
+  }
+
+  SECTION("Divide equals") {
+    Lexer lexer("/=");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::DIVIDE_EQUALS);
+  }
+
+  SECTION("Plus plus") {
+    Lexer lexer("++");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::PLUS_PLUS);
+  }
+
+  SECTION("Minus minus") {
+    Lexer lexer("--");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::MINUS_MINUS);
+  }
+
+  SECTION("Bitwise and") {
+    Lexer lexer("&");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::BIT_AND);
+  }
+
+  SECTION("Bitwise or") {
+    Lexer lexer("|");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::BIT_OR);
+  }
+
+  SECTION("Bitwise xor") {
+    Lexer lexer("^");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::BIT_XOR);
+  }
+
+  SECTION("Bitwise not") {
+    Lexer lexer("~");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::BIT_NOT);
+  }
+
+  SECTION("Left bit shift") {
+    Lexer lexer("<<");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::LBIT_SHIFT);
+  }
+
+  SECTION("Right bit shift") {
+    Lexer lexer(">>");
+    auto tokens = lexer.scan_tokens();
+
+    REQUIRE(tokens[0].type == TokenType::RBIT_SHIFT);
+  }
 }
 
 TEST_CASE("Lexer tokenizes types", "[lexer]") {
   SECTION("Integer Types") {
-    SECTION("int and uint") {
-      auto types = GENERATE("int", "uint");
-      auto token_types = GENERATE(TokenType::I32, TokenType::U32);
-      Lexer lexer(types);
+    SECTION("I8") {
+      Lexer lexer("i8");
       auto tokens = lexer.scan_tokens();
 
-      REQUIRE(tokens[0].type == token_types);
+      REQUIRE(tokens[0].type == TokenType::I8);
     }
 
-    SECTION("Signed Integers") {
-      auto types = GENERATE("i8", "i16", "i32", "i64");
-      auto token_types = GENERATE(TokenType::I8, TokenType::I16, TokenType::I32,
-                                  TokenType::I64);
-      Lexer lexer(types);
+    SECTION("I16") {
+      Lexer lexer("i16");
       auto tokens = lexer.scan_tokens();
 
-      REQUIRE(tokens[0].type == token_types);
+      REQUIRE(tokens[0].type == TokenType::I16);
     }
 
-    SECTION("Unsigned Integers") {
-      auto types = GENERATE("u8", "u16", "u32", "u64");
-      auto token_types = GENERATE(TokenType::U8, TokenType::U16, TokenType::U32,
-                                  TokenType::U64);
+    SECTION("I32") {
+      auto types = GENERATE("int", "i32");
       Lexer lexer(types);
       auto tokens = lexer.scan_tokens();
 
-      REQUIRE(tokens[0].type == token_types);
+      REQUIRE(tokens[0].type == TokenType::I32);
+    }
+
+    SECTION("I64") {
+      Lexer lexer("i64");
+      auto tokens = lexer.scan_tokens();
+
+      REQUIRE(tokens[0].type == TokenType::I64);
+    }
+
+    SECTION("u8") {
+      Lexer lexer("u8");
+      auto tokens = lexer.scan_tokens();
+      REQUIRE(tokens[0].type == TokenType::U8);
+    }
+
+    SECTION("U16") {
+      Lexer lexer("u16");
+      auto tokens = lexer.scan_tokens();
+
+      REQUIRE(tokens[0].type == TokenType::U16);
+    }
+
+    SECTION("U32") {
+      auto types = GENERATE("uint", "u32");
+      Lexer lexer(types);
+      auto tokens = lexer.scan_tokens();
+
+      REQUIRE(tokens[0].type == TokenType::U32);
+    }
+
+    SECTION("U64") {
+      Lexer lexer("u64");
+      auto tokens = lexer.scan_tokens();
+
+      REQUIRE(tokens[0].type == TokenType::U64);
     }
   }
 
