140625dea897553fe91af5d67c34b8e4cc50f437
Ethan Hanks <ethanjhanks99@gmail.com>
2026-05-16 20:59:30 -0600

    initialized classes with methods I think I'll need

--- src/parser/ASTNode.hpp
+++ src/parser/ASTNode.hpp
@@ -6,4 +6,10 @@ class ASTNode {
 public:
   ASTNode(Token token);
   ASTNode *add_child(Token token);
+  ASTNode *get_parent();
+
+private:
+  Token m_token;
+  ASTNode *m_child;
+  ASTNode *m_parent;
 };
