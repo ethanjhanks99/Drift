76d872f667fdcd56ae134c1b160ece32f134fee2
Ethan Hanks <ethanjhanks99@gmail.com>
2026-06-17 07:43:18 -0600

    program node

--- src/tools/AST.hpp
+++ src/tools/AST.hpp
@@ -2,8 +2,14 @@
 
 #include "tools/NodeType.hpp"
 #include "tools/SourceLocation.hpp"
+#include <memory>
+#include <vector>
 
 struct AST {
   NodeType type;
   SourceLocation loc;
+};
+
+struct Program : AST {
+  std::vector<std::unique_ptr<AST>> top_level_decls;
 };
